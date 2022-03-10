0bb6837a0a591834a2677e55f03bd79786feb9f1
Trupti Kanase <trupti_kanase@epam.com>
2022-03-10 22:49:46 +0530

    third commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World, welcome!!";
+		return "Hello World, welcome..this is second commit!!";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
